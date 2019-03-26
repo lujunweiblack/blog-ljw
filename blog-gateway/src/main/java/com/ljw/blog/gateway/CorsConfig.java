af863b211bc043272be4bf77f8d62669474601af
lujunwei_black@163.com <lujunwei_black@163.com>
2019-03-26 22:15:22 +0800

    首页假数据完成

--- blog-gateway/src/main/java/com/ljw/blog/gateway/CorsConfig.java
+++ blog-gateway/src/main/java/com/ljw/blog/gateway/CorsConfig.java
@@ -1,9 +1,66 @@
 package com.ljw.blog.gateway;
 
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
 /**
  * @author: lujunwei
  * @time: 9:54 2019/3/21
  * @des:
  */
 public class CorsConfig {
+
+//    private static final String MAX_AGE = "18000";
+//
+//    @Bean
+//    public WebFilter corsFilter() {
+//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+//            ServerHttpRequest request = ctx.getRequest();
+//            if (CorsUtils.isCorsRequest(request)) {
+//                ServerHttpResponse response = ctx.getResponse();
+//                HttpHeaders headers = response.getHeaders();
+//                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+//                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token,auth-token");
+//                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "POST, GET, PUT, DELETE");
+//                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+//                headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
+//                headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, MAX_AGE);
+//                if (request.getMethod() == HttpMethod.OPTIONS) {
+//                    response.setStatusCode(HttpStatus.OK);
+//                    return Mono.empty();
+//                }
+//
+//            }
+//            return chain.filter(ctx);
+//        };
+//    }
+//
+//    @Bean
+//    public ServerCodecConfigurer serverCodecConfigurer() {
+//        return new DefaultServerCodecConfigurer();
+//    }
+//
+//    /**
+//     * 如果使用了注册中心（如：Eureka），进行控制则需要增加如下配置
+//     */
+//    @Bean
+//    public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient,
+//                                                                        DiscoveryLocatorProperties properties) {
+//        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+//    }
 }
