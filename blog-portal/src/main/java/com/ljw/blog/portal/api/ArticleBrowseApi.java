59bc5c441c353c23464bf1553c9d9ea30dac7ba1
lujunwei@hyron.onaliyun.com <lujunwei@hyron.onaliyun.com>
2019-05-24 14:25:48 +0800

    v-2 add  blog browse number

--- blog-portal/src/main/java/com/ljw/blog/portal/api/ArticleBrowseApi.java
+++ blog-portal/src/main/java/com/ljw/blog/portal/api/ArticleBrowseApi.java
@@ -1,5 +1,7 @@
 package com.ljw.blog.portal.api;
 
+import com.ljw.blog.common.model.BArticleBrowse;
+
 /**
  * @author: lujunwei
  * @time: 16:55 2019/4/10
@@ -7,4 +9,9 @@ package com.ljw.blog.portal.api;
  */
 public interface ArticleBrowseApi {
     Integer articleBrowseQueryCount(long articleId);
+
+    int articleBrowseInsert(BArticleBrowse articleBrowse);
+
+    Integer articleBrowseDistinct(BArticleBrowse articleBrowse);
 }
+
--- blog-portal/src/main/java/com/ljw/blog/portal/mapper/ArticleBrowseMapper.java
+++ blog-portal/src/main/java/com/ljw/blog/portal/mapper/ArticleBrowseMapper.java
@@ -1,14 +1,19 @@
 package com.ljw.blog.portal.mapper;
 
+import com.ljw.blog.common.model.BArticleBrowse;
 import org.apache.ibatis.annotations.Select;
+import tk.mybatis.mapper.common.Mapper;
 
 /**
  * @author: lujunwei
  * @time: 17:03 2019/4/10
  * @des:
  */
-public interface ArticleBrowseMapper {
+public interface ArticleBrowseMapper extends Mapper<BArticleBrowse> {
 
     @Select(" select count(1) from b_article_browse t where t.article_id= #{articleId}")
     Integer articleBrowseQueryCount(long articleId);
+
+    @Select(" select count(1) from b_article_browse t where t.article_id= #{articleId} and t.client_ip = #{clientIp} and t.client_id = #{clientId} and t.client_name = #{clientName} and DATE_FORMAT(t.create_date,'%Y-%m-%d') = DATE_FORMAT(SYSDATE(),'%Y-%m-%d') ")
+    Integer articleBrowseDistinct(BArticleBrowse articleBrowse);
 }
